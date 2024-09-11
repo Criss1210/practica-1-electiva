fc394282d7b91fd7d513b3f153bb6fa14af83ada
User <User@DESKTOP-3P5G92E>
2024-09-11 11:31:50 -0400

    Agregar archivos de proyecto.

---
+++ practica 1 electiva/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace practica_1_electiva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
